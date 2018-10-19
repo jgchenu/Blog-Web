390c8b49e99088244d35fb05d803daf7d70320dd
jgchenu <jgchenu@foxmail.com>
2018-10-19 18:49:28 +0800

    使用redux 存取全局的管理员数据，用户数据

--- src/pages/aboutMe/index.jsx
+++ src/pages/aboutMe/index.jsx
@@ -1,29 +1,13 @@
 import React from "react";
-import api from "@/lib/api";
-const { person } = api;
+import { connect } from "react-redux";
+@connect(state => state.admin)
 class User extends React.Component {
-  state = { content: "" };
-  componentWillMount() {
-    this.loadData();
-  }
-  loadData = () => {
-    this.$axios({
-      url: `${person}`,
-      method: "get"
-    }).then(res => {
-      if (res.data.code === 200) {
-        this.setState({
-          content: res.data.data.introduction
-        });
-      }
-    });
-  };
   render() {
     return (
       <div>
         <div
           className="personMessage"
-          dangerouslySetInnerHTML={{ __html: this.state.content }}
+          dangerouslySetInnerHTML={{ __html: this.props.introduction }}
         />
       </div>
     );

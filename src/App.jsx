8da9035bfc6607a1aa875849f4c6d16cc0a63123
jgchenu <jgchenu@foxmail.com>
2018-10-26 21:42:46 +0800

    移除了canvas

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,11 @@ import NavLeft from "./components/navLeft/index";
 import MyHeader from "./components/header/index";
 import "./styles/App.less";
 import MHeader from "@/components/mHeader";
-import CanvasNest from "canvas-nest.js";
 const { Content, Sider } = Layout;
-const config = {
-  color: "0,0,0",
-  count: 88,
-  opacity:.8
-};
+
 
 class App extends React.Component {
   state = {};
-  componentDidMount() {
-    var element = document.getElementById("root");
-    this.cn = new CanvasNest(element, config);
-  }
-  componentWillUnmount() {
-    this.cn.destroy();
-  }
   render() {
     return (
       <Layout className="container">

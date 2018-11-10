7947dc7194b5abc7ee25251cf2fc8af8591ddaf5
jgchenu <jgchenu@foxmail.com>
2018-11-11 02:34:54 +0800

    更新了getParam

--- src/lib/getParam.js
+++ src/lib/getParam.js
@@ -2,6 +2,7 @@ import history from "@/router/history";
 export default (key) => {
     let value = '';
     if (history.location.search) {
+        console.log(history.location.search)
         let newArr = [];
         let arr = history.location.search.substr(1).split('&');
         arr.forEach(item => {
@@ -9,11 +10,12 @@ export default (key) => {
         })
         let index = newArr.indexOf(key);
         value = newArr[index + 1];
+        console.log(newArr)
     }
     let obj = {
-        'page': 1,
-        'keyword': ''
+        'page': value || 1,
+        'keyword': (value) || ''
     }
-    value = value || obj[key];
+    value = obj[key];
     return value;
 }
